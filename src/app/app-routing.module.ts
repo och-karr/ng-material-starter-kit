671ed79955bd5e69714a0a6c626933304b59b95c
och-karr <okaa93@gmail.com>
2022-11-19 22:19:00 +0100

    feat: implement crypto chips list

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,17 +1,17 @@
-import {NgModule} from '@angular/core';
-import {RouterModule} from '@angular/router';
-import {ProductListComponent} from './components/product-list/product-list.component';
-import {CategoryListComponent} from './components/category-list/category-list.component';
-import {ProductListComponentModule} from './components/product-list/product-list.component-module';
-import {ProductsServiceModule} from './services/products.service-module';
-import {CategoryListComponentModule} from './components/category-list/category-list.component-module';
-import {CategoriesServiceModule} from './services/categories.service-module';
+import { NgModule } from '@angular/core';
+import { RouterModule } from '@angular/router';
+import { ProductListComponent } from './components/product-list/product-list.component';
+import { CategoryListComponent } from './components/category-list/category-list.component';
+import { CryptoChipListComponent } from './components/crypto-chip-list/crypto-chip-list.component';
+import { ProductListComponentModule } from './components/product-list/product-list.component-module';
+import { ProductsServiceModule } from './services/products.service-module';
+import { CategoryListComponentModule } from './components/category-list/category-list.component-module';
+import { CategoriesServiceModule } from './services/categories.service-module';
+import { CryptoChipListComponentModule } from './components/crypto-chip-list/crypto-chip-list.component-module';
+import { CryptoChipsServiceModule } from './services/crypto-chips.service-module';
 
 @NgModule({
-  imports: [RouterModule.forRoot([{path: 'products', component: ProductListComponent}, {
-    path: 'categories',
-    component: CategoryListComponent
-  }]), ProductListComponentModule, ProductsServiceModule, CategoryListComponentModule, CategoriesServiceModule],
+  imports: [RouterModule.forRoot([{ path: 'products', component: ProductListComponent }, { path: 'categories', component: CategoryListComponent }, { path: 'crypto', component: CryptoChipListComponent }]), ProductListComponentModule, ProductsServiceModule, CategoryListComponentModule, CategoriesServiceModule, CryptoChipListComponentModule, CryptoChipsServiceModule],
   exports: [RouterModule]
 })
 export class AppRoutingModule {
