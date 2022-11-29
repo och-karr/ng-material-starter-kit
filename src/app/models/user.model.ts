23469168a9f8652e20495ffe05935042a4ec18e7
och-karr <okaa93@gmail.com>
2022-11-29 14:16:23 +0100

    refactor: change user model

--- src/app/models/user.model.ts
+++ src/app/models/user.model.ts
@@ -1,28 +1,22 @@
 export interface UserModel {
-  id: number;
+  readonly id: number;
   readonly email: string;
   readonly username: string;
   readonly password: string;
-  readonly name: Array<Name>;
-  readonly address: Array<Address>;
+  name: {
+    readonly firstname: string;
+    readonly lastname: string;
+  };
+  readonly address: {
+    readonly city: string,
+    readonly street: string,
+    readonly number: number,
+    readonly zipcode: string,
+    readonly geolocation: {
+      readonly lat: string;
+      readonly long: string;
+    };
+  };
   readonly phone: string;
-}
-
-interface Name {
-  readonly firstname: string;
-  readonly lastname: string;
-}
-
-interface Address {
-  readonly city: string,
-  readonly street: string,
-  readonly number: number,
-  readonly zipcode: string,
-  readonly geolocation: Array<Geolocation>;
-}
-
-interface Geolocation {
-  readonly lat: string;
-  readonly long: string;
 }
 
